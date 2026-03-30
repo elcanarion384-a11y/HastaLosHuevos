e730b918c88c7da532f8ea31ee8a1c8c83407410
Tesla <elcanarion384@gmail.com>
2026-03-30 20:04:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -299,5 +299,5 @@ if __name__ == "__main__":
         print(f"Base de datos encontrada: {DB_PATH}")
 
     print("Servidor iniciando en http://localhost:5000")
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
